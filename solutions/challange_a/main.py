13c22ed3beea18de8fd8cd1033c44d4bee9454d6
Pedro Netto <pedronetto31415@gmail.com>
2024-05-08 14:53:46 -0300

    fix: EOF

--- solutions/challange_a/main.py
+++ solutions/challange_a/main.py
@@ -1,24 +1,24 @@
+import sys
+
 def is_valid_input(bottles_count, step):
     return (
         bottles_count > 1 and bottles_count < 5 * (10 ** 4) and
         step > 1 and step < 5 * (10 ** 3)
     )
-    
-while EOFError: 
-    bottles_count, step = map(int, input().split(" "))
 
+for line in sys.stdin:
+    bottles_count, step = map(int, line.split(" "))
+    
     if not is_valid_input(bottles_count, step):
         print('Erro')
         exit(-1)
-
+        
     boxed_bottles = [i for i in range(bottles_count)]
     
     result = []
-    
     i = 0
     while boxed_bottles:
         i = (i + step - 1) % len(boxed_bottles)
         result.append(boxed_bottles.pop(i))
         
     print(result[len(result) - 1] + 1)
-
